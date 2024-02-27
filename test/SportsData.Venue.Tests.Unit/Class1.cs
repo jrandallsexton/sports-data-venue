a3148e94f12e07d65e759929a461891868de2e16
Randall Sexton <jrandallsexton@gmail.com>
2024-02-27 18:59:24 -0500

    prepping azure pipeline

--- test/SportsData.Venue.Tests.Unit/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SportsData.Venue.Tests.Unit
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ test/SportsData.Venue.Tests.Unit/DevOpsTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace SportsData.Venue.Tests.Unit
+{
+    public class DevOpsTests
+    {
+        [Fact]
+        public void TrueShouldBeTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}

eeb87f3e80ec9e014f8244f40ca2ceb671ec5659
Randall Sexton <jrandallsexton@gmail.com>
2024-02-27 19:10:28 -0500

    forgot to enable swagger for testing only

--- src/SportsData.Venue/Program.cs
+++ src/SportsData.Venue/Program.cs
@@ -17,11 +17,11 @@ namespace SportsData.Venue
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
